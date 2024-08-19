a6cd9b25e4d88417da60c79b65801fc0630ac35f
ahmmukhlis <mutis20139@gmail.com>
2024-08-19 09:24:58 +0700

    Trial Python

---
+++ Trial1.py
@@ -0,0 +1,10 @@
+print ("Klewwang")
+import numpy as np
+x = [(1,2,3), (4,5,6), (1,2,3)]
+print (np.array(x))
+print (x[2])
+
+a = ['ab', 'ab', 'ac', 'ad', 'ae']
+a[1:3]=[1, 2, 3, 4, 5]
+print (a)
+
